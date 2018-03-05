0c30ba9af75a96b82433e9c1c90369d45a7fe33b
chiraggoyal0714 <chirag.goyal0714@gmail.com>
2018-03-05 15:21:24 +0530

    Error resolved

--- login.php
+++ login.php
@@ -1,87 +1,54 @@
 <?php
 include 'default.php';
 session_start();
-$value = $_POST['sign'];
-if($value == 1){
-    login();
-}
-else{
-    signup();
-}
+$name = $_GET["name"];
+$fbid = $_GET["id"];
+$email = $_GET["email"];
 
+exists($email,$fbid);
 
 function signup(){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $cpassword = $_POST['cpassword'];
+    $name = $_GET["name"];
+    $fbid = $_GET["id"];
+    $email = $_GET["email"];
     $db = getDB();
-    if($cpassword == $password) {
-        $count = exists($username);
-        if($count > 0){
-            echo "<script type='text/javascript'>alert('Username Exists'); location='usersignup.php';</script>";
-        }
-        else{
-            $stmt=$db->prepare('INSERT INTO users (username,password,email) VALUES (:username,:password,:email)');
-            $stmt->bindValue(':username', $username);
-            $stmt->bindValue(':password', $password);
-            $stmt->bindValue(':email', $email);
-            $stmt->execute();
-            $stmt=$db->prepare('SELECT * FROM users WHERE username = :username and email = :email ' );
-            $stmt->bindValue(':username', $username);
-            $stmt->bindValue(':email', $email);
-            $stmt->execute();
-            $query = $stmt->fetch();
-            $id = $query['id'];
-            $stmt=$db->prepare('INSERT INTO qstns_users (users_id) VALUES (:id)');
-            $stmt->bindValue(':id', $id);
-            $stmt->execute();
-            echo "<script type='text/javascript'>alert('Successfully Sign Up');location='userlogin.php'</script>";
-        }
-    }
-    else{
-        echo "<script type='text/javascript'>alert('Password Not Match'); location='usersignup.php';</script>";
-    }
-
+    $stmt=$db->prepare('INSERT INTO users (username,email,fbid) VALUES (:username,:email,:fbid)');
+    $stmt->bindValue(':username', $name);
+    $stmt->bindValue(':email', $email);
+    $stmt->bindValue(':fbid', $fbid);
+    $stmt->execute();
+    $stmt=$db->prepare('SELECT * FROM users WHERE fbid = :fbid and email = :email' );
+    $stmt->bindValue(':fbid', $fbid);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    $query = $stmt->fetch();
+    $id = $query['id'];
+    $stmt=$db->prepare('INSERT INTO qstns_users (users_id) VALUES (:id)');
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+    $_SESSION['fbid']=$fbid;
+    $_SESSION['email'] = $email;
+    $_SESSION['name'] = $name;
+    header('location:college.php');
 }
 
-function login(){
-    $username = $_POST['name'];
-    $password = $_POST['password'];
-    $db = getDB();
-    if($username='admin' && $password='admin!davinci2k18'){
-        $_SESSION['admin']=1;
-        $_SESSION['id'] = 0;
-        header('location:admin.php');
-    }
-    else{
-        $stmt=$db->prepare('SELECT * FROM users WHERE username = :username and password = :password ' );
-        $stmt->bindValue(':username', $username);
-        $stmt->bindValue(':password', $password);
-        $stmt->execute();
-        $login = $stmt->fetchall();
-        $count = count($login);
-        echo $count;
-        if($count != 0 ){
-            $_SESSION['username']=$username;
-            $_SESSION['id'] = $login[0]['id'];
-            header('location:page.php');
-        }
-        else{
-            echo "<script type='text/javascript'>alert('Invalid Username or Password'); location='userlogin.php';</script>";
-        }
-    }
 
-}
 
-function exists($username){
-    $username = $username;
+function exists($email, $fbid){
     $db = getDB();
-    $stmt=$db->prepare('SELECT * FROM users WHERE username = :username' );
-    $stmt->bindValue(':username', $username);
+    $stmt=$db->prepare('SELECT * FROM users WHERE fbid = :fbid and email = :email' );
+    $stmt->bindValue(':fbid', $fbid);
+    $stmt->bindValue(':email', $email);
     $stmt->execute();
     $login = $stmt->fetchall();
     $count = count($login);
-    return $count;
+    if ($count != 0){
+        $_SESSION['fbid']=$fbid;
+        $_SESSION['id'] = $login[0]['id'];
+        header("location:page.php");
+    }
+    else{
+        signup();
+    }
 }
 ?>
--- loginstatus.php
+++ loginstatus.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$username = isset($_SESSION['username']) ? $_SESSION['username'] : null;
-$loginStatus = !is_null($username) && isset($username) ? 2 : 1;
+$fbid = isset($_SESSION['fbid']) ? $_SESSION['fbid'] : null;
+$loginStatus = !is_null($fbid) && isset($fbid) ? 2 : 1;
 if ($loginStatus == 1) {
     echo "<script type='text/javascript'>alert('Login First'); location='index.html';</script>";
     die();
